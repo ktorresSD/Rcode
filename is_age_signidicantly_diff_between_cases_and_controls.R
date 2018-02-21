6951acffba0081db19f28e04184a95af88b6e630
Katy <ktorresSD@users.noreply.github.com>
2018-02-21 14:39:21 -0800

    Add files via upload

--- is_age_signidicantly_diff_between_cases_and_controls.R
+++ is_age_signidicantly_diff_between_cases_and_controls.R
@@ -7,25 +7,27 @@
 #load required libraries
 library(plyr)
 #Replace this path with location where data is currently stored
-setwd('C:/Users/Psychiatry Lab/Documents/GWAS_Freeze2_CURRENT/0_GWAS PAPER TABLE 1 PROJECT')
+setwd('C:/Users/Psychiatry Lab/Documents/GWAS_Freeze2_CURRENT/000_FREEZE_2_PAPER/0_TABLE 1')
 
 
 #READ DATA
 #------------------------------------------------------------------------
-dataset0 <- read.csv('Table1_master_without_hatmap.csv',header=T,na.strings=c(NA,999))
+dataset0 <- read.csv('master_fam_file_without_hapmap_USED_FOR_TABLE_1.csv',header=T,na.strings=c(NA,999))
 
 
 #DATA CLEANING PRIOR TO TEST
 #--------------------------------------------------------------------------------------
 table(dataset0$DX)
+table(dataset0$Study)
 
 #exclude all entries where DX is missing == -9
 datasetwithdx <- dataset0[which(dataset0$DX=='1' | dataset0$DX=='2'), ]
 #check exclusion worked
 table(datasetwithdx$DX)
+table(datasetwithdx$Study)
 
 ##EXCLUDE ALL STUDIES THAT HAVE NO CONTROLS or AGES (THEY WILL PRODUCE ERROR IN T.TEST FUCNTION)
- dataset <- datasetwithdx[!datasetwithdx$Study %in% c("FSCD","COGA", "COGB", "Bryant IVS and fire", "VETSA", "CHOICE AND OPT", "DCS Rothbaum", "Risborough" ), ]
+ dataset <- datasetwithdx[!datasetwithdx$Study %in% c("FSCD","WMH","COGA", "COGB", "CHOICE AND OPT", "DCS Rothbaum", "Risborough" ), ]
  table(dataset$Study)
  dataset$Study <- droplevels(dataset$Study)
  
@@ -36,9 +38,15 @@ table(datasetwithdx$DX)
 
 #ONE STUDY DONE FOR VALIDATING FUNCTION
 #------------------------------------
+ # #t test for one as validation of function
+ # nhs22 <- dataset[which(dataset$Study=="NHS2_2"),]
+ # ttest01<- t.test(nhs22$AGE ~ nhs22$DX)
+ # ttest01
+ # ttest01$p.value
+ 
 #t test for one as validation of function
 baker <- dataset[which(dataset$Study=="Baker"),]
-ttest0<- t.test(baker$AGE, baker$DX)
+ttest0<- t.test(baker$AGE ~ baker$DX)
 ttest0
 ttest0$p.value
 
@@ -51,24 +59,26 @@ mean(bakercases$AGE,  na.rm=TRUE)
 #FUNCTION TO RUN THROUGH ALL STUDIES
 #------------------------------------
 
-#split by study first
-split.dataset<- split(dataset,dataset$Study, drop=FALSE)
-
-
-df<-sapply(split.dataset, function(x) {
-#want to see if age is significantly different
-ttest<- t.test(x$AGE,x$DX)
-return(ttest$p.value)
-})
+# #split by study first
+# split.dataset<- split(dataset,dataset$Study, drop=FALSE)
+# 
+# 
+# df<-sapply(split.dataset, function(x) {
+# #want to see if age is significantly different
+# ttest<- t.test(x$AGE~ x$DX)
+# return(ttest$p.value)
+# })
 
 # #OR DO THIS INSTEAD OF SPLIT FUNCTION
 
 ttestfun <- function(df)
 {
-  t.test(df$AGE,df$DX)$p.value
+  t.test(df[which(df$DX ==1),]$AGE,df[which(df$DX ==2),]$AGE)$p.value
+  #t.test(df$AGE~df$DX)$p.value
+ # print(table(df$DX))
 }
 DF2<-ddply(dataset, ~Study,ttestfun)
 
-write.csv(df,  "C:/Users/Psychiatry Lab/Documents/GWAS_Freeze2_CURRENT/0_GWAS PAPER TABLE 1 PROJECT/age_difference_test_20180216.csv",quote=T,row.names=TRUE,na="#N/A")
+#write.csv(df,  "C:/Users/Psychiatry Lab/Documents/GWAS_Freeze2_CURRENT/0_GWAS PAPER TABLE 1 PROJECT/age_difference_test_20180216.csv",quote=T,row.names=TRUE,na="#N/A")
 
-write.csv(DF2,  "C:/Users/Psychiatry Lab/Documents/GWAS_Freeze2_CURRENT/0_GWAS PAPER TABLE 1 PROJECT/age_difference_test_20180216_v2.csv",quote=T,row.names=TRUE,na="#N/A")
+write.csv(DF2,  "~/GWAS_Freeze2_CURRENT/000_FREEZE_2_PAPER/0_TABLE 1/age_difference_test_20180221.csv",quote=T,row.names=TRUE,na="#N/A")
