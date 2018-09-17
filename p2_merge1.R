ca5211de50e668205775fb6f006cf53d5debe589
Katy <ktorresSD@users.noreply.github.com>
2018-09-17 15:31:22 -0700

    Merging all phenotype files together into master
    Read all files in directory, store them as individual dataframes. Select only desired variables. Combine all phenotype files together to make one dataframe of all files.

--- p2_merge1.R
+++ p2_merge1.R
@@ -7,7 +7,7 @@
               #if they dont exist, create them and fill them with -9
               #rbing columns together for a full p2 list
 ##########################################################################################
-
+library(data.table)
 #STEPS TO TAKE:
 #read in data
 #select only specified columns
@@ -32,22 +32,22 @@ ls()
 
 #make list of dataframes
 #NOT INCLUDING p2_bya.csv, p2_nss.csv,  p2_ppds.csv
-list1<- list(p2_adnh.csv,                 p2_bake.csv,        
+list1<- list(p2_adnh_updated_20180910.csv, p2_bake_updated_20180910.csv,        
 p2_betr.csv,         p2_betr_control.csv, p2_boba.csv,        
 p2_bry2.csv ,        p2_coga.csv,         p2_cogb.csv,        
 p2_com1.csv,         p2_dcsr.csv,         p2_defe.csv,        
 p2_dnhs.csv,         p2_eacr.csv,         p2_eghs.csv ,       
-p2_feen.csv,         p2_fscd.csv,         p2_ftcb.csv,        
+p2_feen.csv,         p2_fscd.csv,         p2_ftcb_updated_20180910.csv,        
 p2_gali.csv,         p2_grac.csv,         p2_gsdc.csv,        
-p2_gtpc.csv,         p2_guts.csv,         p2_kmct.csv ,       
+p2_gtpc_updated_20180914.csv,         p2_guts.csv,         p2_kmct.csv ,       
 p2_ksud.csv,         p2_mrsc.csv,         p2_ncmh.csv ,       
-p2_nhrv.csv,         p2_nhs2.csv,         p2_nhsy.csv ,       
+p2_nhrv_updated_20180910.csv,         p2_nhs2.csv,         p2_nhsy.csv ,       
 p2_niut.csv,         p2_onga.csv ,       
-p2_port.csv,         p2_pris.csv ,       
+p2_port_updated_20180910.csv,         p2_pris.csv ,       
 p2_prom.csv,         p2_ring.csv,         p2_safr.csv ,       
 p2_satu.csv,         p2_seep.csv ,        p2_shrs.csv ,       
 p2_stro.csv,         p2_teic.csv ,        p2_vets.csv ,       
-p2_vris.csv,         p2_wach.csv,         p2_wang.csv ,       
+p2_vris_update_20180914.csv,         p2_wach.csv,         p2_wang_updated_20180910.csv ,       
 p2_yehu.csv)
 
 #COMBINES LIST OF DATAFRAMES INTO 1 
@@ -57,15 +57,11 @@ p2list$phenotype_id<- p2list$IID
 dim(p2list) #65889
 names(p2list)
 
-#the following shold be dichotomous
+#the following should be dichotomous
 table(p2list$Sex) #STRONGSTAR has some missing SEX info
 table(p2list$Case) 
 table(p2list$Current_PTSD_Dx)
-table(p2list$Lifetime_PTSD_Dx) #not, recode FTCB
+table(p2list$Lifetime_PTSD_Dx)
 
-#check what study has Lifetime_PTSD_Dx issues
-ltptsdfix<-p2list[p2list$Lifetime_PTSD_Dx==0,]
-table(ltptsdfix$study_abbrev)
-
-# #read in and binds together all files but only works with equal number of columns
-# do.call(rbind, lapply(list.files(path='.', pattern="p2_"), read.csv, header=TRUE))
+#UNUSED OTHER OPTION -- read in and binds together all files but only works with equal number of columns
+#do.call(rbind, lapply(list.files(path='.', pattern="p2_"), read.csv, header=TRUE))
